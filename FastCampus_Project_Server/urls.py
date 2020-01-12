638a5b1166b1062ae8dfc7218b9233ef58c23970
changja88 <DJdnls88!!>
2020-01-13 00:34:06 +0900

    save

--- FastCampus_Project_Server/urls.py
+++ FastCampus_Project_Server/urls.py
@@ -1,4 +1,4 @@
-"""FastCampu_Project_Server URL Configuration
+"""FastCampus_Project_Server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- FastCampus_Project_Server/wsgi.py
+++ FastCampus_Project_Server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FastCampu_Project_Server project.
+WSGI config for FastCampus_Project_Server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FastCampu_Project_Server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FastCampus_Project_Server.settings')
 
 application = get_wsgi_application()
